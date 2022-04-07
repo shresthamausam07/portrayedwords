60477bcb5e1e6c739087fd7f297f9ad58c9c818a
shresthamausam07 <65083338+shresthamausam07@users.noreply.github.com>
2022-04-07 21:32:43 +0545

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,8 +4,8 @@ module.exports = {
     es2021: true,
   },
   extends: [
-    'plugin:react/recommended',
-    'airbnb',
+    //...
+    'plugin:@next/next/recommended',
   ],
   parserOptions: {
     ecmaFeatures: {
